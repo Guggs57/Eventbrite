0baf5b96761cb7bd5648e6db9154957bed535376
Guggs57 <n_guggenbuhl@hotmail.com>
2025-04-13 19:10:54 +0200

    début des pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  get "home/index"
-  get "users/index"
-  get "events/index"
   resources :users, only: [:index]
   resources :events, only: [:index]
   resources :attendances, only: [:index]
