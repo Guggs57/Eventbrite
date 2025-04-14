2e1f1502e720b7ac5c8bbfc3826d635464b035da
Guggs57 <n_guggenbuhl@hotmail.com>
2025-04-14 20:48:28 +0200

    mise en place de devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :users, only: [:index]
+  resources :users, only: [:new, :create, :show]
   resources :events, only: [:index]
   resources :attendances, only: [:index]
 
