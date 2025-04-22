39a521f33fc1b0bff9975d9f7bcd87f51a22c191
Guggs57 <n_guggenbuhl@hotmail.com>
2025-04-22 20:30:36 +0200

    mise en place de la "route"  stripe

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,5 +3,5 @@ class Event < ApplicationRecord
     has_many :attendees, through: :attendances, source: :user 
     belongs_to :creator, class_name: 'User'
 
-    validates :name, presence: true
+    validates :title, presence: true
 end
